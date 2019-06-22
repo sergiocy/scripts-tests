9ed59f843af453dc9cde2646b01da1bcb81cf2eb
Sergio Córdoba Yanguas <scordoba@atsistemas.com>
2019-06-22 12:37:47 +0200

    adding tests

---
+++ python/hpc-book/simulator/Particle.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jan 13 14:02:32 2019
+
+@author: sergio
+"""
+
+class Particle:
+    def __init__(self, x, y, ang_vel):
+        self.x = x
+        self.y = y
+        self.ang_vel = ang_vel
+        print('particle created \n')
+        
+        
+        
+    def get_x(self):
+        print('returning x: ' + str(self.x))
+        return(self.x)
+    
+    def get_y(self):
+        print('returning y: ' + str(self.y))
+        return(self.y)
+        
+    def get_ang_vel(self):
+        print('returning angular velocity: ' + str(self.ang_vel))
+        return(self.ang_vel)     
+        
+    def set_x(self, x):
+        print('setting x: ' + str(x))
+        self.x = x
+    
+    def set_y(self, y):
+        print('setting y: ' + str(y))
+        self.y = y
+        
+    def set_ang_vel(self, ang_vel):
+        print('setting angular velocity: ' + str(ang_vel))
+        self.ang_vel = ang_vel 
+        
---
+++ python/hpc-book/simulator/main_simulator.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jan 13 13:10:13 2019
+
+@author: sergio
+"""
+
+from Particle import Particle
+
+
+if __name__ == '__main__':
+    print('creating particle')
+    
+    p1 = Particle(1,1,3)
+    print(p1.get_x())
+    
+    
