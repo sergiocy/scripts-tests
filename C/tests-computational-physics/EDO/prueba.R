5900155f4277fb8a3f2377ee946ff954694de18d
Sergio Córdoba Yanguas <scordoba@atsistemas.com>
2019-07-07 09:36:06 +0200

    first numeric paticles simulator

--- C/tests-computational-physics/EDO/prueba.R
+++
@@ -1,8 +0,0 @@
-#prueba para ejecutar desde consola
-#comenzado: 18/10/2015
-
-txtPrueba <- "C:\\Users\\Usuario\\Desktop\\HPC\\FISICA_COMPUTACIONAL\\Ch2-EDO\\posiciones.txt"
-datos <- read.table(txtPrueba) 
-plot(datos$V1, datos$V2, xlab="horizontal", ylab="altura")
-
- 
