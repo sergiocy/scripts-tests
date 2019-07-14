3bcfebe32e9e10ca0900a22d06e770e41a694680
Sergio Córdoba Yanguas <scordoba@atsistemas.com>
2019-07-14 11:13:17 +0200

    cleaning C folder

--- C/tests-computational-physics/Distribuciones-1/distrib/archivo.cmd
+++
@@ -1,5 +0,0 @@
-@echo off
-
-echo load'C:/Users/Usuario/Desktop/HPC/FISICA_COMPUTACIONAL/Ch4-Distribuciones/distrib/plotearGNU.txt' | gnuplot -persist
-
-exit
--- C/tests-computational-physics/Distribuciones-2/distrib/archivo.cmd
+++
@@ -1,5 +0,0 @@
-@echo off
-
-echo load'C:/Users/Usuario/Desktop/HPC/FISICA_COMPUTACIONAL/Ch5-Distribuciones/distrib/plotearGNU.txt' | gnuplot -persist
-
-exit
