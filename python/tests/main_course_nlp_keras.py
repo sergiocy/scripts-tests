fa5a92965a0cb6d330195b806b7ff5fd0a790955
Sergio Córdoba Yanguas <scordoba@atsistemas.com>
2019-09-11 23:09:57 +0200

    course nlp-keras

--- python/tests/main_course_nlp_keras.py
+++ python/tests/main_course_nlp_keras.py
@@ -3,14 +3,15 @@
 import os
 import sys
 import numpy as np
+import pandas as pd
 
 from service.utils import create_logger
 
 
 
 
-PATH_DATA = '../../../data/input/jigsaw-toxic-comment-classification-challenge/train.csv'
-PATH_LOG_FILE = '../../../log/log_course_nlp_keras.log'
+PATH_DATA = '../../../0-data/input/jigsaw-toxic-comment-classification-challenge/train.csv'
+PATH_LOG_FILE = '../../../0-log/log_course_nlp_keras.log'
 
 
 
@@ -18,8 +19,14 @@ PATH_LOG_FILE = '../../../log/log_course_nlp_keras.log'
 if __name__=='__main__':
 
     logger = create_logger(PATH_LOG_FILE)
-    logger.info('path_data: ' + path_data)
-    logger.info('path_log: ' + path_data)
+    logger.info('path_data: ' + PATH_DATA)
+    logger.info('path_log: ' + PATH_LOG_FILE)
+
+    data = pd.read_csv(PATH_DATA, sep=',', encoding='utf-8')
+    print(data.head)
+    print(data.shape)
+
+
 
 
     
