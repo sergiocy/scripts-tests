46afa733244af5a65ebbb7dc0761312b718d5f89
Sergio Córdoba Yanguas <scordoba@atsistemas.com>
2019-11-03 21:10:25 +0100

    submodules test

---
+++ python/tests/main_course_efficient_ml.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+import numpy as np
+import pandas as pd
+
+#from service.utils import create_logger
+
+
+
+#PATH_DATA = '../../../0-data/input/jigsaw-toxic-comment-classification-challenge/train.csv'
+#PATH_LOG_FILE = '../../../0-log/log_course_nlp_keras.log'
+
+
+
+
+if __name__=='__main__':
+
+    #logger = create_logger(PATH_LOG_FILE)
+    #logger.info('path_data: ' + PATH_DATA)
+    #logger.info('path_log: ' + PATH_LOG_FILE)
+
+    #data = pd.read_csv(PATH_DATA, sep=',', encoding='utf-8')
+    #print(data.head)
+    #print(data.shape)
+
+    print("efficient ML course tests")
+
+
+
+
+    
