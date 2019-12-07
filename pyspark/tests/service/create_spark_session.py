9b5def52bbc756bd3c7790e32840130dc8352479
Sergio Córdoba Yanguas <scordoba@atsistemas.com>
2019-12-07 12:07:21 +0100

    changes in folders structure

--- pyspark/tests/service/create_spark_session.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from pyspark.sql import SparkSession
-
-
-def create_spark_session(appName='test'):
-    spark = SparkSession.builder.appName(appName).getOrCreate()    
-
-    return spark
